12b4f0f1d8a62191cbad4a7f9e02d173b875ce51
Tahmid Hossen <tahmid.hossen@nexdecade.com>
2024-07-01 18:02:00 +0600

    working on navabr

--- app/(global)/parental-lock/page.js
+++ app/(global)/parental-lock/page.js
@@ -1,85 +1,9 @@
-import React,  from 'react';
-import Image from "next/image";
-// import React, { useState } from 'react';
 
+import ParentalLock from "@/components/ParentalLock";
 const ParentalLockPage = () => {
   return (
-      <div>
-          <div className="container">
-              <button
-                  className="w-[200px] mt-4 text-2xl text-black bg-transparent dark:text-white hover:bg-white hover:bg-opacity-10 flex items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                      <path d="M12 2L1 21h22L12 2zm0 5v3.5h-3v2h3V17h-3v2h3.5c.3 0 .5-.2.5-.5v-9c0-.3-.2-.5-.5-.5h-3z"/>
-                  </svg>
-                  Parental Lock
-              </button>
-          </div>
-          <div className="mt-20 rounded-[40px] bg-zinc-300 h-screen"
-               style={{boxShadow: '0px 8px 20px rgba(0,0,0,0.5) inset'}}>
-              <div className="flex justify-center items-center">
-                  <Image src="/images/parental-lock/parental-lock.png" alt="" width={130} height={124}
-                         className="w-[130px] h-[124px] mt-[-60px] pb-[32px]"/>
-              </div>
-              <div className="flex justify-center items-center">
-                  <div id="parentalLockContainer"
-                       className="flex flex-col items-center gap-[1.5rem] w-full md:w-[40%] px-2 text-center">
-                      <p className="text-[22px] text-black dark:text-white md:text-2xl font-bold">Lock Content with your
-                          personal PIN</p>
-                      <p className="text-[25px] text-black md:text-[16px] font-semibold dark:text-[#D1D1D1] py-[24px]">Will
-                          anyone
-                          under the age of 18 be watching restricted content?</p>
-                      <button
-                          className="btn rounded-[8px] uppercase bg-[#C1141E] h-[48px] px-14 text-white text-lg border-none hover:bg-[#b4111b]">Lock
-                          Now
-                      </button>
-                      <p className="text-[14px] leading-5 text-center font-semibold text-black dark:text-[#D1D1D1] pt-[24px]">Click
-                          Lock Now to set up Personal PIN</p>
-                  </div>
-              </div>
-
-              <div className="flex flex-col items-center gap-19 w-full md:w-40% px-2 text-center">
-                  <p className="text-lg font-bold text-black dark:text-white pb-[20px]">Set Your Parental Lock PIN</p>
-                  <div className="w-380px">
-                      <input type="password" defaultValue="PIN" placeholder="PIN"
-                             className="w-full p-2 mb-[20px] text-center text-black bg-gray-500 rounded-md dark:text-white bg-opacity-20 placeholderColorWhite"/>
-                      <button
-                          className="rounded-md uppercase px-2 h-[48px] bg-[#C1141E] w-full text-white border-none hover:bg-[#b4111b]">Confirm
-                      </button>
-                  </div>
-              </div>
-
-
-              <div className="flex flex-col items-center w-full md:w-40% px-2 text-center">
-                  <p className="text-lg font-bold text-black dark:text-white pb-[24px]">Set Your Email Address</p>
-                  <div className="w-380px text-left">
-                      <label htmlFor="email" className="text-black dark:text-white text-sm font-normal text-16px">Enter
-                          E-mail Address</label>
-                      <input type="email" placeholder="Enter your email"
-                             className="w-full p-3 px-4 my-[24px] text-sm text-white bg-gray-500 rounded-md bg-opacity-20 placeholderColorWhite"/>
-                      <button
-                          className="rounded-md px-2 h-[48px] bg-[#C1141E] w-full text-white border-none hover:bg-[#b4111b]">Submit
-                      </button>
-                  </div>
-              </div>
-
-
-              <div className="flex flex-col items-center w-full md:w-40% px-2 text-center">
-                  <p className="text-lg font-bold text-black dark:text-white">Set Your Email Address</p>
-                  <div className="w-380px text-left">
-                      <input type="email" placeholder="Enter your verification code"
-                             className="w-full p-3 px-4 my-[24px] text-sm text-white bg-gray-500 rounded-md bg-opacity-20 placeholderColorWhite"/>
-                      <button
-                          className="rounded-md px-2 h-[48px] bg-[#C1141E] w-full text-white border-none hover:bg-[#b4111b]">Complete
-                      </button>
-                  </div>
-              </div>
-
-
-          </div>
-      </div>
-
-  )
-      ;
+      <ParentalLock/>
+  );
 };
 
 export default ParentalLockPage;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -32,45 +32,6 @@ import {Button} from "@/components/ui/button";
 import { useState } from 'react';
 
 
-
-const components = [
-  {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
-]
-
 const movieCategories = [
   "Hollywood", "Bollywood", "Animation", "Foreign", "Franch", "Chinese",
   "Indian Bangla", "Italian", "Japanese", "Korean", "Malayalam", "Russia",
@@ -97,18 +58,18 @@ export default function Navbar () {
         <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem>
-              <NavigationMenuTrigger>
+              <NavigationMenuTrigger className="transition ease-in-out hover:text-red-700 dark:hover:text-red-700">
                 <Link href='/movies'>
                   Movies
                 </Link>
               </NavigationMenuTrigger>
               <NavigationMenuContent>
                 {/*<ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">*/}
-                <ul className="grid w-[400px] gap-4 p-6 md:w-[600px] md:grid-cols-4 lg:w-[600px]">
+                <ul className="grid gap-4 p-6 md:w-[650px] lg:w-[650px] lg:grid-cols-4">
                   {movieCategories.map((category, index) => (
                       <li key={index}
-                          className="transition ease-in-out py-1 px-2 rounded-[5px] hover:text-[#E83431] hover:scale-105 hover:font-semibold duration-300">
-                        <Link href={`/${category}`} className="text-black dark:text-white">
+                          className="transition ease-in-out py-1 px-2 rounded-[5px] hover:text-red-700 dark:hover:text-red-700 hover:scale-105 hover:font-semibold duration-300">
+                        <Link href={`/${category}`} className="text-black dark:text-white hover:text-red-700 dark:hover:text-red-700">
                           {category}
                         </Link>
                       </li>
@@ -124,15 +85,16 @@ export default function Navbar () {
                 </Link>
               </NavigationMenuTrigger>
               <NavigationMenuContent>
-                <ul className="grid w-[400px] gap-4 p-6 md:w-[600px] md:grid-cols-4 lg:w-[600px]">
+                <ul className="grid gap-4 p-6 md:w-[650px] lg:w-[650px] lg:grid-cols-4">
                   {movieCategories.map((category, index) => (
                       <li key={index}
-                          className="transition ease-in-out py-1 px-2 rounded-[5px] hover:text-[#E83431] hover:scale-105 hover:font-semibold duration-300">
-                          <Link href={`/${category}`} className="text-black dark:text-white">
-                            {category}
-                          </Link>
-                        </li>
-                    ))}
+                          className="transition ease-in-out py-1 px-2 rounded-[5px] hover:text-red-700 dark:hover:text-red-700 hover:scale-105 hover:font-semibold duration-300">
+                        <Link href={`/${category}`}
+                              className="text-black dark:text-white hover:text-red-700 dark:hover:text-red-700">
+                          {category}
+                        </Link>
+                      </li>
+                  ))}
                 </ul>
               </NavigationMenuContent>
             </NavigationMenuItem>
@@ -143,15 +105,15 @@ export default function Navbar () {
                 </Link>
               </NavigationMenuTrigger>
               <NavigationMenuContent>
-                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                  {components.map((component) => (
-                    <ListItem
-                      key={component.title}
-                      title={component.title}
-                      href={component.href}
-                    >
-                      {component.description}
-                    </ListItem>
+                <ul className="grid gap-4 p-6 md:w-[650px] lg:w-[650px] lg:grid-cols-4">
+                  {movieCategories.map((category, index) => (
+                      <li key={index}
+                          className="transition ease-in-out py-1 px-2 rounded-[5px] hover:text-red-700 dark:hover:text-red-700 hover:scale-105 hover:font-semibold duration-300">
+                        <Link href={`/${category}`}
+                              className="text-black dark:text-white hover:text-red-700 dark:hover:text-red-700">
+                          {category}
+                        </Link>
+                      </li>
                   ))}
                 </ul>
               </NavigationMenuContent>
@@ -378,17 +340,17 @@ export default function Navbar () {
         </div>
 
 
-        <div>
-          <Link href='/login'>
-            <Button variant="none" size="icon">
-              <svg width="20" height="20" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M7.5 0.875C5.49797 0.875 3.875 2.49797 3.875 4.5C3.875 6.15288 4.98124 7.54738 6.49373 7.98351C5.2997 8.12901 4.27557 8.55134 3.50407 9.31167C2.52216 10.2794 2.02502 11.72 2.02502 13.5999C2.02502 13.8623 2.23769 14.0749 2.50002 14.0749C2.76236 14.0749 2.97502 13.8623 2.97502 13.5999C2.97502 11.8799 3.42786 10.7206 4.17091 9.9883C4.91536 9.25463 6.02674 8.87499 7.49995 8.87499C8.97317 8.87499 10.0846 9.25463 10.8291 9.98831C11.5721 10.7206 12.025 11.8799 12.025 13.5999C12.025 13.8623 12.2376 14.0749 12.5 14.0749C12.7623 14.075 12.975 13.8623 12.975 13.6C12.975 11.72 12.4778 10.2794 11.4959 9.31166C10.7244 8.55135 9.70025 8.12903 8.50625 7.98352C10.0187 7.5474 11.125 6.15289 11.125 4.5C11.125 2.49797 9.50203 0.875 7.5 0.875ZM4.825 4.5C4.825 3.02264 6.02264 1.825 7.5 1.825C8.97736 1.825 10.175 3.02264 10.175 4.5C10.175 5.97736 8.97736 7.175 7.5 7.175C6.02264 7.175 4.825 5.97736 4.825 4.5Z"
-                  fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path>
-              </svg>
-            </Button>
-          </Link>
-        </div>
+        {/*<div>*/}
+        {/*  <Link href='/login'>*/}
+        {/*    <Button variant="none" size="icon">*/}
+        {/*      <svg width="20" height="20" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">*/}
+        {/*        <path*/}
+        {/*          d="M7.5 0.875C5.49797 0.875 3.875 2.49797 3.875 4.5C3.875 6.15288 4.98124 7.54738 6.49373 7.98351C5.2997 8.12901 4.27557 8.55134 3.50407 9.31167C2.52216 10.2794 2.02502 11.72 2.02502 13.5999C2.02502 13.8623 2.23769 14.0749 2.50002 14.0749C2.76236 14.0749 2.97502 13.8623 2.97502 13.5999C2.97502 11.8799 3.42786 10.7206 4.17091 9.9883C4.91536 9.25463 6.02674 8.87499 7.49995 8.87499C8.97317 8.87499 10.0846 9.25463 10.8291 9.98831C11.5721 10.7206 12.025 11.8799 12.025 13.5999C12.025 13.8623 12.2376 14.0749 12.5 14.0749C12.7623 14.075 12.975 13.8623 12.975 13.6C12.975 11.72 12.4778 10.2794 11.4959 9.31166C10.7244 8.55135 9.70025 8.12903 8.50625 7.98352C10.0187 7.5474 11.125 6.15289 11.125 4.5C11.125 2.49797 9.50203 0.875 7.5 0.875ZM4.825 4.5C4.825 3.02264 6.02264 1.825 7.5 1.825C8.97736 1.825 10.175 3.02264 10.175 4.5C10.175 5.97736 8.97736 7.175 7.5 7.175C6.02264 7.175 4.825 5.97736 4.825 4.5Z"*/}
+        {/*          fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path>*/}
+        {/*      </svg>*/}
+        {/*    </Button>*/}
+        {/*  </Link>*/}
+        {/*</div>*/}
 
       </div>
     </div>
