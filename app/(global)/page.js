1088bf143e06a4049357cfb26d580ce18e297f92
mahmud886 <iqbal886mahmud@gmail.com>
2024-07-01 11:21:08 +0600

    api handler added... to app

--- app/(global)/page.js
+++ app/(global)/page.js
@@ -1,6 +1,9 @@
+import {getApplicationSettings} from "@/serivces/api-service";
 
 
-export default function Home() {
+export default async function Home() {
+  const appConfig =  await getApplicationSettings();
+  console.log(appConfig);
   return (
     <div className='container'>
       <h2>Hello</h2>
