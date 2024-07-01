08d5961816a872aea09dce6b1dd1a7436fa8430f
Tahmid Hossen <tahmid.hossen@nexdecade.com>
2024-07-01 17:35:08 +0600

    working on navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -71,6 +71,14 @@ const components = [
   },
 ]
 
+const movieCategories = [
+  "Hollywood", "Bollywood", "Animation", "Foreign", "Franch", "Chinese",
+  "Indian Bangla", "Italian", "Japanese", "Korean", "Malayalam", "Russia",
+  "Tamil", "Thailand", "Turkey", "Hong-Kong", "Spanish", "Pakistani",
+  "Germany", "3D Movie", "Brasil", "Persian", "Hindi Dubbed", "IMDB Top 250",
+  "Panjabi", "UHD Movies"
+];
+
 export default function Navbar () {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
@@ -95,35 +103,18 @@ export default function Navbar () {
                 </Link>
               </NavigationMenuTrigger>
               <NavigationMenuContent>
-                <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                  <li className="row-span-3">
-                    <NavigationMenuLink asChild>
-                      <a
-                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                        href="/"
-                      >
-
-                        <div className="mb-2 mt-4 text-lg font-medium">
-                          shadcn/ui
-                        </div>
-                        <p className="text-sm leading-tight text-muted-foreground">
-                          Beautifully designed components that you can copy and
-                          paste into your apps. Accessible. Customizable. Open
-                          Source.
-                        </p>
-                      </a>
-                    </NavigationMenuLink>
-                  </li>
-                  <ListItem href="/docs" title="Introduction">
-                    Re-usable components built using Radix UI and Tailwind CSS.
-                  </ListItem>
-                  <ListItem href="/docs/installation" title="Installation">
-                    How to install dependencies and structure your app.
-                  </ListItem>
-                  <ListItem href="/docs/primitives/typography" title="Typography">
-                    Styles for headings, paragraphs, lists...etc
-                  </ListItem>
+                {/*<ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">*/}
+                <ul className="grid w-[400px] gap-4 p-6 md:w-[600px] md:grid-cols-4 lg:w-[600px]">
+                  {movieCategories.map((category, index) => (
+                      <li key={index}
+                          className="transition ease-in-out py-1 px-2 rounded-[5px] hover:text-[#E83431] hover:scale-105 hover:font-semibold duration-300">
+                        <Link href={`/${category}`} className="text-black dark:text-white">
+                          {category}
+                        </Link>
+                      </li>
+                  ))}
                 </ul>
+                {/*</ul>*/}
               </NavigationMenuContent>
             </NavigationMenuItem>
             <NavigationMenuItem>
@@ -133,16 +124,15 @@ export default function Navbar () {
                 </Link>
               </NavigationMenuTrigger>
               <NavigationMenuContent>
-                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                  {components.map((component) => (
-                    <ListItem
-                      key={component.title}
-                      title={component.title}
-                      href={component.href}
-                    >
-                      {component.description}
-                    </ListItem>
-                  ))}
+                <ul className="grid w-[400px] gap-4 p-6 md:w-[600px] md:grid-cols-4 lg:w-[600px]">
+                  {movieCategories.map((category, index) => (
+                      <li key={index}
+                          className="transition ease-in-out py-1 px-2 rounded-[5px] hover:text-[#E83431] hover:scale-105 hover:font-semibold duration-300">
+                          <Link href={`/${category}`} className="text-black dark:text-white">
+                            {category}
+                          </Link>
+                        </li>
+                    ))}
                 </ul>
               </NavigationMenuContent>
             </NavigationMenuItem>
